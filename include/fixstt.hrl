41bb2924d3b642e766343e1f584fa89d58924a95
Mariano Guerra <luismarianoguerra@gmail.com>
2014-11-25 15:12:57 +0100

    provide accessors and constructors

--- include/fixstt.hrl
+++ include/fixstt.hrl
@@ -1,4 +1,4 @@
 -define(FIXSTT_DEFAULT_ENTRY_SIZE_BYTES, 512).
 
--record(fixstt, {id, lat, lng, len, date, ref, type, msg}).
+-record(fixstt, {id, lat, lng, len, date, ref=0, type=0, msg}).
 
--- src/fixstt.erl
+++ src/fixstt.erl
@@ -1,5 +1,6 @@
 -module(fixstt).
--export([to_binary/1, to_binary/2, from_binary/1, now_ms/0, now_ms_fast/0, format/1]).
+-export([to_binary/1, to_binary/2, from_binary/1, now_ms/0, format/1, get/2,
+         set/3, new/1, new/3]).
 
 -include("fixstt.hrl").
 
@@ -34,12 +35,30 @@ format(#fixstt{id=Id, lat=Lat, lng=Lng, date=Date, len=Len, ref=Ref, type=Type, 
                   [Id, Lat, Lng, Date, Len, Ref, Type, Msg]).
 
 now_ms() ->
-    {Mega, Sec, Micro} = erlang:now(),
-    ((Mega * 1000000 + Sec) * 1000000 + Micro) div 1000.
-
-now_ms_fast() ->
     {Mega, Sec, Micro} = os:timestamp(),
     ((Mega * 1000000 + Sec) * 1000000 + Micro) div 1000.
+
+get(#fixstt{id=Val}, id) -> Val;
+get(#fixstt{lat=Val}, lat) -> Val;
+get(#fixstt{lng=Val}, lng) -> Val;
+get(#fixstt{date=Val}, date) -> Val;
+get(#fixstt{ref=Val}, ref) -> Val;
+get(#fixstt{type=Val}, type) -> Val;
+get(#fixstt{msg=Val}, msg) -> Val.
+
+set(Entry, id, Val) -> Entry#fixstt{id=Val};
+set(Entry, lat, Val) -> Entry#fixstt{lat=Val};
+set(Entry, lng, Val) -> Entry#fixstt{lng=Val};
+set(Entry, date, Val) -> Entry#fixstt{date=Val};
+set(Entry, ref, Val) -> Entry#fixstt{ref=Val};
+set(Entry, type, Val) -> Entry#fixstt{type=Val};
+set(Entry, msg, Val) -> Entry#fixstt{msg=Val}.
+
+new(Msg) -> new(Msg, 9001, 9001).
+
+new(Msg, Lat, Lng) ->
+    #fixstt{msg=Msg, lat=Lat, lng=Lng, date=now_ms(), len=size(Msg)}.
+
 
 pad_to_entry_size(Binary, EntrySize) ->
     case size(Binary) of
